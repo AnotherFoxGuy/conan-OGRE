c66f9eaec7724c34c1f49850788d24cd6ce713a0
Edgar <Edgar@AnotherFoxGuy.com>
2019-08-09 15:33:29 +0200

    :wrench: Add libzzip-dev

--- conanfile.py
+++ conanfile.py
@@ -23,6 +23,7 @@ class OGREConan(ConanFile):
                 installer.install("libx11-dev")
                 installer.install("libxt-dev")
                 installer.install("libxaw7-dev")
+                installer.install("libzzip-dev")
 
     def requirements(self):
         if os_info.is_windows:
